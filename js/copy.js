d57698a70f47bde3abc4307c163e6547356a2a81
Querzzis <who.danya@icloud.com>
2024-04-02 21:44:15 +0800

    upddd

--- js/copy.js
+++ js/copy.js
@@ -1,4 +1,4 @@
 function myFunction() {
-    navigator.clipboard.writeText('C:\\Games\\Lethal Company V50 (Schizo)\\BepInEx\\plugins')
+    navigator.clipboard.writeText('C:\\Games\\Lethal Company V50 (Schizo)\\BepInEx')
     alert("Текст скопирован!")
 }
